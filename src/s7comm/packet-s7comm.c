74d15dc66522e0df81143d024fb549a373b1f440
thomas_v2 <thomas_v2@45802b70-cb78-474c-a225-0ca69f292a50>
2012-10-25 20:24:02 +0000

    comments update

--- src/s7comm/packet-s7comm.c
+++ src/s7comm/packet-s7comm.c
@@ -1,24 +1,29 @@
 /* packet-s7comm.c
-*
-* Author:		Thomas Wiens, 2011 (th.wiens@gmx.de)
-* Version:		0.0.2
-* Description:	Wireshark dissector for S7-Communication
-*
-*
-* This program is free software; you can redistribute it and/or
-* modify it under the terms of the GNU General Public License
-* as published by the Free Software Foundation; either version 2
-* of the License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-*/
+ *
+ * Author:		Thomas Wiens, 2011 (th.wiens@gmx.de)
+ * Version:		0.0.2
+ * Description:	Wireshark dissector for S7-Communication
+ *
+ * $Id$
+ *
+ * Wireshark - Network traffic analyzer
+ * By Gerald Combs <gerald@wireshark.org>
+ * Copyright 1998 Gerald Combs
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 
 #ifdef HAVE_CONFIG_H
 # include "config.h"
