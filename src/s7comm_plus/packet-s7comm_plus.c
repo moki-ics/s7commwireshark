e9b6a3d01dd08c78f9efaf0e1622c8fe1f2cbb5d
thomas_v2 <thomas_v2@45802b70-cb78-474c-a225-0ca69f292a50>
2014-10-15 21:29:58 +0000

    Modified the handling of the unknown data in the beginning of the login write
    With the assumption that the unknown part has 2 bytes in the beginning
    declaring the number of values which follow as varuint32.
    
    From: Juergen Kosel <klj@softing.com>

--- src/s7comm_plus/packet-s7comm_plus.c
+++ src/s7comm_plus/packet-s7comm_plus.c
@@ -1063,15 +1063,28 @@ s7commp_decode_data_request_write(tvbuff_t *tvb,
         offset += 27;
 
     } else {
-        guint8 bytesToSkip;
+        guint8 unknownValues1;
+        guint8 unknownValues2;
+        guint8 unknownValuesRead;
 
         proto_tree_add_text(tree, tvb, offset-4, 4, "Different Write Request with first value !=0 : 0x%08x. TODO", value);
         /* n Bytes unbekannt, im ersten davon steht vermutlich n/2 */
-        bytesToSkip = tvb_get_guint8(tvb, offset);
-        bytesToSkip *= 2;
-        bytesToSkip += 2;
-        proto_tree_add_bytes(tree, hf_s7commp_data_data, tvb, offset, bytesToSkip, tvb_get_ptr(tvb, offset, bytesToSkip));
-        offset += bytesToSkip;
+        unknownValues1 = tvb_get_guint8(tvb, offset);
+        proto_tree_add_text(tree, tvb, offset, 1, "unknown values count 1: %u", unknownValues1);
+        offset +=1;
+        unknownValues2 = tvb_get_guint8(tvb, offset);
+        proto_tree_add_text(tree, tvb, offset, 1, "unknown values count 2: %u", unknownValues2);
+        offset +=1;
+        for(unknownValuesRead=0;
+            (unknownValuesRead < unknownValues1) && (offset < offsetmax);
+            unknownValuesRead++)
+        {
+            guint8 octet_count = 0;
+            gint32 int32val = tvb_get_varint32(tvb, &octet_count, offset);
+            proto_tree_add_text(tree, tvb, offset, octet_count, "unknown value[%d]: 0x%08x : %d",
+                                unknownValuesRead, int32val, int32val);
+            offset += octet_count;
+        }
         // the begin of remaining part could be decoded simliar to the start session stuff:
         return s7commp_decode_session_stuff(tvb,tree,offset,offsetmax);
     }
