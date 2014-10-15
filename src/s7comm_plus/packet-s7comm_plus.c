74bc077c8b37e56522b8dc6c3fbc52bd0fcd2438
thomas_v2 <thomas_v2@45802b70-cb78-474c-a225-0ca69f292a50>
2014-10-15 21:33:25 +0000

    The data width of type 0x11 with flag 0x80 is 1 byte
    From: Juergen Kosel <klj@softing.com>

--- src/s7comm_plus/packet-s7comm_plus.c
+++ src/s7comm_plus/packet-s7comm_plus.c
@@ -769,6 +769,14 @@ s7commp_decode_value(tvbuff_t *tvb,
             offset += 8;
         }
         break;
+    case 0x11:
+        g_strlcpy(str_val, "Unknown Type", sizeof(str_val));
+        if(datatype_flags == 0x80) {
+             // unknown type but length is known as 1 bytes
+            length_of_value = 1;
+            offset += 1;
+        }
+        break;
     default:
         /* zur Zeit unbekannter Typ, muss abgebrochen werden solange der Aufbau nicht bekannt */
         g_strlcpy(str_val, "Unknown Type", sizeof(str_val));
